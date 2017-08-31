e2c976360c6996fee9623f2d45a113fdba7e0a05
Te Standby <testandby@gmail.com>
2017-08-31 02:01:35 +0200

    Modularizin basic multilingual enums

--- src/index/com/front/decoder.ts
+++ src/index/com/front/decoder.ts
@@ -7,48 +7,6 @@ import {O} from "lowbar/main"
 
 type Char = string
 
-//    Super kind:
-export enum SprKind {
-    Control = 0xc,
-    //…   Functional char
-    Seper = 0x5,
-    //…   Seperator
-    Oper = 0x0,
-    //…   Operator
-    Liter = 0x1,
-    //…   Literal char
-}
-
-export enum Kind {
-    Nothing = SprKind.Control << 0x4,
-    Break = SprKind.Seper << 0x4 | 0xb,
-    White = SprKind.Seper << 0x4,
-    Punc = SprKind.Oper << 0x4,
-    Sign = SprKind.Oper << 0x4 | 0x5,
-    Num = SprKind.Oper << 0x4 | 0x1,
-    Letter = SprKind.Oper << 0x4 | 0xa,
-}
-
-export enum Latin4 {
-    Null = Kind.Nothing << 0x10 | 0x00,
-    Tab = Kind.White << 0x10,
-    Feed = Kind.Break << 0x10,
-    Return = Kind.Break << 0x10,
-
-    Space = Kind.White << 0x10,
-    Bang = Kind.Punc << 0x10,
-    Quote = Kind.Punc << 0x10,
-    Hash = Kind.Sign << 0x10,
-
-    Dollar = Kind.Letter << 0x10,
-    Percent = Kind.Sign << 0x10,
-    Ampersand = Kind.Sign << 0x10,
-    Apostrophe = Kind.Punc << 0x10,
-    //TODO Complete
-}
-
-console.log(Kind.Break)
-
 function decode(
     chunk$ :$<string>
 ) :$<number> {
@@ -64,12 +22,10 @@ function decode(
         // TODO   Anyhack. Include typings providing `String.prototype.atCodePoint`!
         //…   converts to numbers representing these code points.
 
-    const invalPoint$ = point$
-        // TODO   Handle other invalid code points
+    const inval$ :$<number> = point$
         .filter(p=> !!(~0xFFFF & p))
-    // TODO   Better decoding error reporting!
-    invalPoint$.map(report)
 
+    inval$.map(report)
     return point$
     // TODO   Compose with `screen`!
 }
