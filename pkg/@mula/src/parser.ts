1679023bd50b1cc93aa26e720b8d7d2535e20539
Te Standby <testandby@gmail.com>
2017-10-05 05:13:09 +0200

    mula: Durin implementin line treeification

--- pkg/@mula/src/parser.ts
+++ pkg/@mula/src/parser.ts
@@ -5,7 +5,77 @@ import {
 
 const tabIndent = 4 as Int
 
-export enum Chars {
+
+export interface Reduc {
+    lineI :Int
+    indentLvl :Int
+    rootTree :Tree<Int[]>
+}
+
+export function reduc(
+    pay :Reduc,
+    line :Int[],
+): Reduc {
+    const lineI = (pay.lineI + 1) as Int
+    const {indentI, pointI} = countIndent(line)
+    const trimmed = line.slice(pointI)
+    const indentLvl = (indentI / tabIndent | 0) as Int
+    //…rounds down
+    const indentMod = (indentI % tabIndent) as Int
+
+    const lineTree = new Tree(trimmed)
+
+    return {
+        lineI,
+        indentLvl,
+        rootTree,
+    }
+}
+
+function countIndent(line :Int[]) {
+    let indentI = 0 as Int
+    let pointI = 0 as Int
+    line: for (let point of line) switch (point) {
+        case Chars.tab as Int:
+            indentI = (indentI + tabIndent) as Int
+            pointI++
+            break
+        case Chars.space as Int:
+            indentI++
+            pointI++
+            break
+        default:
+            break line
+    }
+
+    return {
+        indentI,
+        pointI,
+    }
+}
+
+export class Tree<E> {
+    constructor (
+        readonly e :E,
+        readonly subnodes :Tree<E>[] = [],
+    ) {}
+
+    replace(patch :{[key :number] :E}): Tree<E> {
+        const subnodes = this.subnodes.map(node=> node.clone())
+        for (let i in patch)
+            if (-1 < Math.sign(i))
+                subnodes[i] = patch[i]
+
+        return new Tree(
+            -1 in patch
+                ? patch[-1]
+                : this.e,
+            subnodes,
+        )
+    }
+}
+
+enum Chars {
     null = 0x00,
     // [control],
     tab = 0x09,
@@ -35,48 +105,4 @@ export enum Chars {
     minus = 0x2d,
     dot = 0x2e,
     slash = 0x2f,
-}
-
-export interface Reduc {
-    lineI :Int
-    indentLvl :Int
-}
-
-export function reduc(
-    pay :Reduc,
-    line :Int[],
-): Reduc {
-    const lineI = pay.lineI + 1 as Int
-    const {indentI, pointI} = countIndent(line)
-    const trimmed = line.slice(pointI)
-    const indentLvl = (indentI / tabIndent | 0) as Int
-    //…rounds down
-    const indentMod = (indentI % tabIndent) as Int
-
-    return {
-        lineI,
-        indentLvl,
-    }
-}
-
-function countIndent(line :Int[]) {
-    let indentI = 0 as Int
-    let pointI = 0 as Int
-    line: for (let point of line) switch (point) {
-        case Chars.tab as Int:
-            indentI = (indentI + tabIndent) as Int
-            pointI++
-            break
-        case Chars.space as Int:
-            indentI++
-            pointI++
-            break
-        default:
-            break line
-    }
-
-    return {
-        indentI,
-        pointI,
-    }
 }
