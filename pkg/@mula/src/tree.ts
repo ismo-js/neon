6941f1f126f357169eade0c81cfc1adb3e3bbf0d
Te Standby <testandby@gmail.com>
2017-10-08 06:24:08 +0200

    mula: Durin typin `tree@skirt`

--- pkg/@mula/src/tree.ts
+++ pkg/@mula/src/tree.ts
@@ -10,14 +10,29 @@ import {
 
 export type Conifer<Lbl> = Tree<Lbl, Lbl>
 
-export interface Generator<In, Out, Res> {
-    next(arg? :In) :Out
+// mapping generator:
+export interface MapGenor<In, Out, Res> {
+    next() :{
+        done :false,
+        value :undefined,
+    }
+    next(arg :In) :{
+        done :false,
+        value :Out,
+    }
+    next(arg :In) :{
+        done :true,
+        value :Res,
+    }
 }
 
-export type Walker<Lbl, Lf extends Lbl, Res, ResLf extends Res> =
-    (lbl: Lbl)=> Generator<Tree<Lbl, Lf>, Tree<Res, ResLf>, Res>
+// walker over a `Tree<In, InLf>`; yieldin a `Tree<Out, OutLf>`:
+export type Walker<In, InLf extends In, Out, OutLf extends Out> =
+    (lvl :Int)=> MapGenor<Tree<In, InLf>,
+          Tree<Out, OutLf>,
+          (lbl :In)=> Out>
 
-// tree graph with branch labels of `Br` and leave labels of `Lf`:
+// tree graph with labels of `Lbl`, in particular leave labels of `Lf`:
 export default class Tree<Lbl, Lf extends Lbl> {
     static Handler = class Handler<Lbl, Lf extends Lbl>
           implements ProxyHandler<Tree<Lbl, Lf>> {
@@ -54,21 +69,25 @@ export default class Tree<Lbl, Lf extends Lbl> {
     }
 
     readonly [key :number] :Tree<Lbl, Lf>
-    length :Int = 0 as Int
 
     protected handler = new Tree.Handler<Lbl, Lf>() 
 
     constructor(
-        label :Lbl,
-        edges :Iterable<Tree<Lbl, Lf>>,
-    )
-    constructor() {
+        lbl :Lbl,
+        edges :Iterable<Tree<Lbl, Lf>> = [],
+    ) {
         return new Px(this, this.handler)
     }
 
-    flatWalk(
-        walker :Iterator<>
-    ) :
+    get length() {
+        return 
+    }
+
+    skirt<Res, ResLf extends Res>(
+        walker :Walker<Lbl, Lf, Res, ResLf>
+    ) :Tree<Res, ResLf> {
+        const genor = walker(this.label)
+    }
 
     protected normalizeI(
         iParam :Int,
