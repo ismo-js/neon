87097e496c8bb16a26d3370adf806b278fa7e3a7
Te Standby <testandby@gmail.com>
2017-10-09 09:20:06 +0200

    mula: Typin `tree` further

--- pkg/@mula/src/tree.ts
+++ pkg/@mula/src/tree.ts
@@ -2,16 +2,15 @@ import {
     INF,
     isInt,
     Int,
+    ITER,
     O,
     Px,
 } from "neon-lowbar"
 
 // ---
 
-export type Conifer<Lbl> = Tree<Lbl, Lbl>
-
-// mapping generator:
-export interface MapGenor<In, Out, Res> {
+// mapping iterator:
+export interface MapIterator<In, Out, Res> {
     next() :{
         done :false,
         value :undefined,
@@ -28,9 +27,11 @@ export interface MapGenor<In, Out, Res> {
 
 // walker over a `Tree<In, InLf>`; yieldin a `Tree<Out, OutLf>`:
 export type Walker<In, InLf extends In, Out, OutLf extends Out> =
-    (lvl :Int)=> MapGenor<Tree<In, InLf>,
+    (lvl :Int)=> MapIterator<Tree<In, InLf>,
           Tree<Out, OutLf>,
-          (lbl :In)=> Out>
+          ((lbl :In)=> Out) | undefined>
+
+export type Conifer<Lbl> = Tree<Lbl, Lbl>
 
 // tree graph with labels of `Lbl`, in particular leave labels of `Lf`:
 export default class Tree<Lbl, Lf extends Lbl> {
@@ -70,23 +71,46 @@ export default class Tree<Lbl, Lf extends Lbl> {
 
     readonly [key :number] :Tree<Lbl, Lf>
 
+    protected UNFIXED = Symbol("{ unfixed value placeholder }")
+    //… not static to enable to:
+    //  store other tree's unfixed symbols in trees in derived classes
     protected handler = new Tree.Handler<Lbl, Lf>() 
+    protected lbl_c :Lbl | symbol = this.UNFIXED
+    protected readonly edges :Iterable<Tree<Lbl, Lf>>
+
+    [Symbol.iterator] :()=> Iterator<Tree<Lbl, Lf>> =
+        this.edges[Symbol.iterator]
+    //… should be [lowbar@ITER] one day,
+    //  but TypeScript's intelligence is limited nowadays.
 
     constructor(
-        lbl :Lbl,
         edges :Iterable<Tree<Lbl, Lf>> = [],
+        fixLbl? :Lbl,
     ) {
+        const hasFixLbl = 2 > arguments.length
+
+        if (hasFixLbl) this.lbl_c = fixLbl
         return new Px(this, this.handler)
     }
 
-    get length() {
-        return 
+    get label() :Lbl {
+        if (this.UNFIXED !== this.lbl_c)
+            return this.lbl_c as Lbl
+        
+        let r: Tree<Lbl, Lf> | Lbl
+        for (r of this);
+        //… iterates over and reads the end result as label:
+        return r! as Lbl
+    }
+
+    get length() :Int {
+        return [...this].length as Int
     }
 
     skirt<Res, ResLf extends Res>(
         walker :Walker<Lbl, Lf, Res, ResLf>
     ) :Tree<Res, ResLf> {
-        const genor = walker(this.label)
+        const iterator = walker(0 as Int)
     }
 
     protected normalizeI(
