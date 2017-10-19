6a44b2ada78250b66c339bbf5c57da794b30ba54
Te Standby <testandby@gmail.com>
2017-10-19 18:42:48 +0200

    mula: Durin stucturin front, Rmin old `tree`

--- pkg/@mula/src/front/async.ts
+++ pkg/@mula/src/front/async.ts
@@ -2,7 +2,10 @@ import {Readable} from "stream"
 
 import {
     default as $,
+    Observable,
+    Listener,
     Stream,
+    Subscription as Subscr,
 } from "xstream"
 
 import {
@@ -21,6 +24,48 @@ import {
 export type Stm<E> = Readable | Stream<E>
 export type CharStmLs = Stm<CharLs>[] | Stm<CharLs>
 
+export type ReadIn = string | Buffer
+export type ReadNext = (input :ReadIn)=> void
+
+export class ReadObservable implements Observable<ReadIn> {
+    constructor(
+        readonly rd :Readable,
+    ) {}
+
+    subscribe(
+        ober :Listener<ReadIn>,
+    ) :Subscr
+    subscribe(
+        next :ReadNext,
+        err :(err :any)=> void,
+        end :()=> void,
+    ) :Subscr
+    subscribe(
+        next :Listener<ReadIn> | ReadNext, 
+        error? :(err :any)=> void,
+        fin? :()=> void,
+    ) :Subscr {
+        const rd = this.rd
+        const ober = 2 > arguments.length
+            ? {
+                next,
+                error,
+                complete: fin,
+            }
+            : next as Listener<ReadIn>
+
+        rd.addListener("data", ober.next as ReadNext)
+        rd.addListener("error", ober.error!)
+        rd.addListener("finish", ober.complete!)
+
+        return {
+            unsubscribe() {
+                rd.removeListener("data", ober)
+            }
+        }
+    }
+} 
+
 export async function parseAsync(
     stms :CharStmLs,
     config :ConfigInter,
@@ -30,8 +75,8 @@ export async function parseAsync(
 
     for (let stmParam of stms) {
         const stm = stmParam instanceof Stream
-            ? $.from(stmParam)
-            : stmParam
+            ? stmParam
+            : $.from(new ReadObservable(stmParam))
     }
 }
 
--- pkg/@mula/src/meta.ts
+++ pkg/@mula/src/meta.ts
@@ -1,38 +1,21 @@
 import {Pm} from "neon-lowbar"
 
-import {
-    Config,
-    ConfigInter,
-    CharLs,
-} from "./front/config"
-import {
-    parse,
-} from "./front/sync"
-import {
-    Stm,
-    CharStmLs,
-    parseAsync,
-    isStm,
-} from "./front/async"
+import {parseAsync} from "./front/async"
 
 // ---
 
 export {
-    Config,
+    default as Config,
     ConfigInter,
+    CharLs,
+} from "./front/config"
+export {
+    parse,
+} from "./front/sync"
+export {
     Stm,
     CharStmLs,
-    CharLs,
-    parse,
     parseAsync,
-}
+} from "./front/async"
 
-export default async function parseAny(
-    val :CharLs | CharStmLs,
-    config :ConfigInter
-) :Pm<Res> {
-    if (isStm(val)
-          || Array.isArray(val) && val.every(isStm))
-        return await parseAsync(val as CharStmLs, config)
-    return parse(val as CharLs, config)
-}
+export default parseAsync
